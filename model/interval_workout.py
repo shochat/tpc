bdc0fb4c8a68d91d5d81f409a3d6884ab1ff50d5
Ilan Shochat <ishochat@cisco.com>
2017-02-13 16:10:00 +0200

    Set parse_detail class method implementing super class abstract method

--- model/interval_workout.py
+++ model/interval_workout.py
@@ -2,44 +2,45 @@ from model.workout import Workout, WorkoutType, Intensity
 
 
 class IntervalWorkout(Workout):
-    def __init__(self, day_in_week, details):
-        self.details = details
+    def __init__(self, day_in_week, raw_details):
+        self.raw_details = raw_details
+        self.details = dict()
         self.parse_details()
         super().__init__(self.details['name'], WorkoutType.INTERVALS, Intensity.UN_AEROBIC, day_in_week, self.duration, self.length)
 
     def parse_details(self):
-        dtl = dict()
-        dtl['name'] = self.details[0]
-        if self.details[1] is not '':
-            dtl['Repeat count'] = self.details[1]
-        if self.details[2] is not '':
-            dtl['Interval distance'] = self.details[2]
-        if self.details[3] is not '':
-            dtl['Interval time'] = self.details[3]
-        if self.details[4] is not '':
-            dtl['Interval pace'] = self.details[4]
-        if self.details[5] is not '':
-            dtl['Recovery distance'] = self.details[5]
-        if self.details[6] is not '':
-            dtl['Recovery time'] = self.details[6]
-        if self.details[7] is not '':
-            dtl['Recovery pace'] = self.details[7]
-        if self.details[8] is not '':
-            dtl['Worm up distance'] = self.details[8]
-        if self.details[9] is not '':
-            dtl['Cool down distance'] = self.details[9]
-        if 'Interval distance' in dtl:
-            total_interval = int(dtl['Repeat count']) * (float(dtl['Interval distance']) / 1000)
+        self.details['name'] = self.raw_details[0]
+        if self.raw_details[1] is not '':
+            self.details['Repeat count'] = self.raw_details[1]
+        if self.raw_details[2] is not '':
+            self.details['Interval distance'] = self.raw_details[2]
+        if self.raw_details[3] is not '':
+            self.details['Interval time'] = self.raw_details[3]
+        if self.raw_details[4] is not '':
+            self.details['Interval pace'] = self.raw_details[4]
+        if self.raw_details[5] is not '':
+            self.details['Recovery distance'] = self.raw_details[5]
+        if self.raw_details[6] is not '':
+            self.details['Recovery time'] = self.raw_details[6]
+        if self.raw_details[7] is not '':
+            self.details['Recovery pace'] = self.raw_details[7]
+        if self.raw_details[8] is not '':
+            self.details['Worm up distance'] = self.raw_details[8]
+        if self.raw_details[9] is not '':
+            self.details['Cool down distance'] = self.raw_details[9]
+        if 'Interval distance' in self.details:
+            total_interval = int(self.details['Repeat count']) * (float(self.details['Interval distance']) / 1000)
         else:
-            total_interval = (int(dtl['Repeat count']) * int(dtl['Interval time'])) / int(dtl['Interval pace'])
-        if 'Recovery distance' in dtl:
-            total_recovery = int(dtl['Repeat count']) * int(dtl['Recovery distance'])
+            total_interval = (int(self.details['Repeat count']) * int(self.details['Interval time'])) / int(
+                self.details['Interval pace'])
+        if 'Recovery distance' in self.details:
+            total_recovery = int(self.details['Repeat count']) * int(self.details['Recovery distance'])
         else:
-            total_recovery = (int(dtl['Repeat count']) * float(dtl['Recovery time'])) / float(dtl['Recovery pace'])
+            total_recovery = (int(self.details['Repeat count']) * float(self.details['Recovery time'])) / float(
+                self.details['Recovery pace'])
 
-        self.length = int(dtl['Worm up distance']) + int(dtl['Cool down distance']) + total_interval + total_recovery
-        self.duration = self.length * ((float(dtl['Interval pace']) + float(dtl['Recovery pace'])) / 2)
-        return dtl
+        self.length = int(self.details['Worm up distance']) + int(self.details['Cool down distance']) + total_interval + total_recovery
+        self.duration = self.length * ((float(self.details['Interval pace']) + float(self.details['Recovery pace'])) / 2)
 
     def __str__(self):
         if 'Interval distance' in self.details:
