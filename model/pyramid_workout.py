696700e221ed57e50fc44c3a9bca42eee3021edb
Ilan Shochat <ishochat@cisco.com>
2017-02-13 16:10:48 +0200

    Implement child PyramidWorkout class methods

--- model/pyramid_workout.py
+++ model/pyramid_workout.py
@@ -3,42 +3,59 @@ from model.workout import Workout, WorkoutType, Intensity
 
 class PyramidWorkout(Workout):
     def __init__(self, day_in_week, details):
-        self.details = details
+        self.raw_details = details
+        self.details = dict()
         self.parse_details()
-        super().__init__(self.details['name'], WorkoutType.INTERVALS, Intensity.UN_AEROBIC, day_in_week, self.duration, self.length)
-
-    def __str__(self):
-        pass
+        super().__init__(self.details['name'], WorkoutType.PYRAMID, Intensity.UN_AEROBIC, day_in_week, self.duration, self.length)
 
     def parse_details(self):
-        dtl = dict()
-        dtl['name'] = self.details[0]
-        if self.details[1] is not '':
-            dtl['Main drill distances'] = self.details[1]
-        if self.details[2] is not '':
-            dtl['Main drill times'] = self.details[2]
-        if self.details[3] is not '':
-            dtl['Main drill paces'] = self.details[3]
-        if self.details[4] is not '':
-            dtl['Recovery distances'] = self.details[4]
-        if self.details[5] is not '':
-            dtl['Recovery times'] = self.details[5]
-        if self.details[6] is not '':
-            dtl['Recovery paces'] = self.details[6]
-        if self.details[7] is not '':
-            dtl['Worm up distance'] = self.details[7]
-        if self.details[8] is not '':
-            dtl['Cool down distance'] = self.details[8]
-
-        if 'Interval distance' in dtl:
-            total_interval = int(dtl['Repeat count']) * (float(dtl['Interval distance']) / 1000)
+        self.details['name'] = self.raw_details[0]
+        if self.raw_details[1] is not '':
+            self.details['Main drill distances'] = [float(x) for x in self.raw_details[1].split('-')]
+        if self.raw_details[2] is not '':
+            self.details['Main drill times'] = [float(x) for x in self.raw_details[2].split('-')]
+        if self.raw_details[3] is not '':
+            self.details['Main drill paces'] = [float(x) for x in self.raw_details[3].split('-')]
+        if self.raw_details[4] is not '':
+            self.details['Recovery distances'] = [float(x) for x in self.raw_details[4].split('-')]
+        if self.raw_details[5] is not '':
+            self.details['Recovery times'] = [float(x) for x in self.raw_details[5].split('-')]
+        if self.raw_details[6] is not '':
+            self.details['Recovery paces'] = [float(x) for x in self.raw_details[6].split('-')]
+        if self.raw_details[7] is not '':
+            self.details['Worm up distance'] = self.raw_details[7]
+        if self.raw_details[8] is not '':
+            self.details['Cool down distance'] = self.raw_details[8]
+        if 'Main drill distances' in self.details:
+            total_pyramid = sum(self.details['Main drill distances'])
         else:
-            total_interval = (int(dtl['Repeat count']) * int(dtl['Interval time'])) / int(dtl['Interval pace'])
-        if 'Recovery distance' in dtl:
-            total_recovery = int(dtl['Repeat count']) * int(dtl['Recovery distance'])
+            total_pyramid = sum(
+                float([time / pace for time, pace in zip(self.details['Main drill times'], self.details['Main drill paces'])]))
+        if 'Recovery distances' in self.details:
+            total_recovery = sum(self.details['Recovery distances'])
         else:
-            total_recovery = (int(dtl['Repeat count']) * float(dtl['Recovery time'])) / float(dtl['Recovery pace'])
+            total_recovery = sum(
+                float([time / pace for time, pace in zip(self.details['Main drill times'], self.details['Main drill paces'])]))
 
-        self.length = int(dtl['Worm up distance']) + int(dtl['Cool down distance']) + total_interval + total_recovery
-        self.duration = self.length * ((float(dtl['Interval pace']) + float(dtl['Recovery pace'])) / 2)
-        return dtl
+        self.length = int(self.details['Worm up distance']) + int(self.details['Cool down distance']) + total_pyramid + total_recovery
+
+        self.duration = self.length * (((sum(self.details['Main drill paces']) / len(self.details['Main drill paces'])) +
+                                        (sum(self.details['Recovery paces']) / len(self.details['Recovery paces']))) / 2)
+
+    def __str__(self):
+        if 'Main drill distances' in self.details:
+            main_drill = '{} KM'.format(self.details['Main drill distances'])
+        else:
+            main_drill = '{} Minutes'.format(self.details['Main drill times'])
+        if 'Recovery distances' in self.details:
+            main_recovery = '{} Km'.format(self.details['Recovery distances'])
+        else:
+            main_recovery = '{} Minutes'.format(self.details['Recovery times'])
+
+        return '*** {} ***\nWorm up for {} KM\nRun for {} in pace {}' \
+               '\nRecovery run for {} in pace {}\nCool down for {} KM'.format(self.details['name'],
+                                                                              self.details['Worm up distance'],
+                                                                              main_drill, self.details['Main drill paces'],
+                                                                              main_recovery, self.details['Recovery paces'],
+                                                                              self.details['Cool down distance']
+                                                                              )
