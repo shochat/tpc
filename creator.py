1c96cc0e922e3a4eafd889f2c8ef21696d823f9d
Ilan Shochat <ishochat@cisco.com>
2017-02-15 15:55:46 +0200

    Clean redundant code while developing and adding TODOs

--- creator.py
+++ creator.py
@@ -1,21 +1,26 @@
 import datetime
-import yaml
-from model.interval_workout import IntervalWorkout
 from model.plan import Plan, RaceDistance
 
 
 def main():
     race_date = datetime.date(2017, 6, 11)
-    p = Plan(race_date=race_date, race_distance=RaceDistance.MARATHON, time_target=1.5, shape_level=7, weekly_training_days=4)
+    p = Plan(race_date=race_date, race_distance=RaceDistance.MARATHON, time_target=3, shape_level=7, weekly_training_days=6)
     p.create()
+    for wp in p.weekly_plan_list:
+        print('--- Weekly plan for week {} ---'.format(wp.weeks_from_race))
+        print('---  Weekly   total  {}  KM ---'.format(wp.weekly_total))
+        for workout in wp.workouts:
+            print(workout)
 
 if __name__ == '__main__':
-    with open('resources/quality-main.yaml', 'r') as data:
-        details = yaml.load(data)['intervals']
-
-    iwo = IntervalWorkout(3, details[1].split(','))
-    print(iwo)
-    # main()
+    main()
 
     # TODO: Check if inserted details enable plan creation according to goal
     # TODO: Set pace in quality workouts according to target pace
+    # TODO: improve equation of calculate workout duration
+    # TODO: Add description field to workout
+    # TODO: Set IntensityLevel for half marathon
+    # TODO: Set quality workouts for half marathon
+    # TODO: Week start day other than Sunday
+    # TODO: calculate target pace and set it into program
+    # TODO: Insert logic to perform Uphill workout only during base and build periods
