702cb7a9c85bba3d62cf59d491eb65c1a79b386c
Ilan Shochat <ishochat@cisco.com>
2017-02-22 17:18:18 +0200

    Add target time to model

--- model/user_details.py
+++ model/user_details.py
@@ -12,9 +12,14 @@ class UserDetails:
             self.race_distance = RaceDistance.HALF_MARATHON
         else:
             self.race_distance = RaceDistance.KM_10
-        self.target_time = datetime.datetime.strptime(form['target_time'], '%H:%M')
+        self.target_time = datetime.datetime.strptime(form['target_time'], '%H:%M').time()
+        self.target_pace = self.calculate_target_pace()
         # TODO former races
         self.age = int(form['age'])
         self.weight = int(form['weight'])
         self.shape_level = int(form['shape_level'])
         self.weekly_training_days = int(form['weekly_training_days'])
+
+    def calculate_target_pace(self):
+        total_time_in_minutes = self.target_time.hour * 60 + self.target_time.minute
+        return total_time_in_minutes / float(self.race_distance.value[0])
