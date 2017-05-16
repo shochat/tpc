b5033603b9af206bcbbc5cb4228e57097223953a
Ilan Shochat <ishochat@cisco.com>
2017-05-16 09:36:11 +0300

    Return UI components

--- controllers.py
+++ controllers.py
@@ -1,5 +1,5 @@
-import json
 from flask import Blueprint
+from flask import render_template
 from flask import request
 from model.plan import Plan
 from model.user_details import UserDetails
@@ -7,9 +7,14 @@ from model.user_details import UserDetails
 controllers = Blueprint('controllers', __name__, template_folder='templates')
 
 
+@controllers.route('/')
+def form():
+    return render_template('index.html')
+
+
 @controllers.route('/create-plan', methods=['POST', 'GET'])
 def create_plan():
     user_details = UserDetails(request.form)
     p = Plan(user_details)
     p.create()
-    return json.dumps(p.serialize())
+    return render_template('results.html', results=p.weekly_plan_list)
---
+++ static/js/main.js
@@ -0,0 +1,13 @@
+$('.datepicker').pickadate({
+    min: true,
+    max: 168,
+    closeOnSelect: true,
+    closeOnClear: true
+});
+$(document).ready(function() {
+    $('select').material_select();
+  });
+
+$(document).ready(function(){
+    $('.collapsible').collapsible();
+  });
